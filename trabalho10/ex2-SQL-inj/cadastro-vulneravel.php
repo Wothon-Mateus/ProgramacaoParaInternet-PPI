fe9f189517dd71199d06f2382b81d89b13de811e
Wothon-Mateus <13wothon@gmail.com>
2024-10-10 16:14:34 -0300

    ex2 finalizado

--- trabalho10/ex2-SQL-inj/cadastro-vulneravel.php
+++ trabalho10/ex2-SQL-inj/cadastro-vulneravel.php
@@ -9,18 +9,36 @@ $telefone = $_POST["telefone"] ?? "";
 try {
 
   // NÃO FAÇA ISSO! Exemplo de código vulnerável a inj. de S-Q-L
-  $sql = <<<SQL
-  INSERT INTO aluno (nome, telefone)
-  VALUES ('$nome', '$telefone');
-  SQL;  
+  // As variáveis $nome e $telefone são inseridas diretamente na consulta SQL,
+  // sem qualquer validação Isso significa que, se um usuário 
+  // mal-intencionado inserir uma entrada com caracteres especiais ou código SQL, 
+  // o banco de dados executará a consulta exatamente como está, incluindo no código 
+  // o usuario malicioso pode por exemplo adicionar esse codigo '); DROP TABLE aluno; --
+
+
+  // $sql = <<<SQL
+  // INSERT INTO aluno (nome, telefone)
+  // VALUES ('$nome', '$telefone');
+  // SQL;
 
   // Experimente fazer o cadastro de um novo aluno preenchendo 
   // o campo telefone utilizando o texto disponibilizado pelo professor
   // nos slides de aula
-  $pdo->exec($sql);
+
+
+  // Prepara a consulta SQL para evitar a vulnerabilidade explicada antes
+  $sql = "INSERT INTO aluno (nome, telefone) VALUES (:nome, :telefone)";
+  $stmt = $pdo->prepare($sql);
+
+  // Associa os valores às variáveis de forma segura
+  $stmt->bindParam(':nome', $nome);
+  $stmt->bindParam(':telefone', $telefone);
+
+  // Executa a consulta
+  $stmt->execute();
+
   header("location: mostra-alunos.php");
   exit();
-} 
-catch (Exception $e) {  
+} catch (Exception $e) {
   exit('Falha ao cadastrar os dados: ' . $e->getMessage());
 }
