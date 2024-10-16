87c1b8f3e718a96fe89d2bff51cc59fd4d8dabc2
Wothon-Mateus <13wothon@gmail.com>
2024-10-16 15:51:30 -0300

    Ex3

--- trabalho11/ex3-recebendo-json/busca-endereco.php
+++ trabalho11/ex3-recebendo-json/busca-endereco.php
@@ -26,3 +26,10 @@ else {
 
 header('Content-type: application/json');
 echo json_encode($endereco);
+
+// Classe Endereco: Define uma estrutura para armazenar dados de endereço, incluindo rua, bairro e cidade.
+// Possui um construtor que inicializa esses valores.
+// Obtendo o CEP: Recebe o CEP a partir de uma requisição HTTP usando $_GET['cep'].
+// Se o valor do CEP for '38400-100', cria um objeto Endereco com informações específicas para esse CEP.
+// Se o valor do CEP for '38400-200', cria um objeto Endereco com outro conjunto de informações.
+// Para qualquer outro CEP, cria um objeto Endereco vazio.
