985c161e8b82833205a8c744a03a12143e96d922
Wothon-Mateus <13wothon@gmail.com>
2024-10-24 12:30:57 -0300

    Finalizado  ate ex 5

--- trabalho12/ex5-infinite-scroll/more-products.php
+++ trabalho12/ex5-infinite-scroll/more-products.php
@@ -16,6 +16,9 @@ class Product
   }
 }
 
+//Uma array $products é criada contendo sete instâncias da classe Product. 
+//Cada produto tem um ID, nome, preço e caminho da imagem.
+
 $products = array(
   new Product(1, 'Smart TV LED 55', 2900, 'images/tv.webp'),
   new Product(2, 'Smartphone 6.5 ABC', 1590, 'images/smartphone.webp'),
@@ -25,6 +28,9 @@ $products = array(
   new Product(6, 'Fone Headset ABC', 250, 'images/headset.webp'),
   new Product(7, 'Pen-drive de 64GB', 90, 'images/pen-drive.webp')
 );
+
+//A variável $randProds armazena um array de cinco produtos escolhidos aleatoriamente da lista de produtos $products. 
+//Para isso, é utilizada a função rand(0, 6), que gera um índice aleatório entre 0 e 6 (correspondente aos índices dos produtos).
  
 $randProds = [
   $products[rand(0, 6)],
@@ -34,5 +40,6 @@ $randProds = [
   $products[rand(0, 6)]
 ];
 
+// O cabeçalho da resposta HTTP é definido como Content-type: application/json para informar que a resposta será enviada no formato JSON.
 header('Content-type: application/json');
 echo json_encode($randProds);
