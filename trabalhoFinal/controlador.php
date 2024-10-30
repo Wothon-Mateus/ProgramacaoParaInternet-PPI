ed36fb31671cd82afc87db799a4b05573116a8bf
Wothon-Mateus <13wothon@gmail.com>
2024-10-30 12:12:39 -0300

    .

--- trabalhoFinal/controlador.php
+++ trabalhoFinal/controlador.php
@@ -16,31 +16,12 @@ switch ($acao) {
 
     try {
       Produto::Create($pdo, $nome, $cpf, $email, $senha, $telefone);
-    //   header("location: produtos.html");
+    //   header("location: cadastro.html");
     } catch (Exception $e) {
       throw new Exception($e->getMessage());
     }
     break;
 
-//   case "excluirProduto":
-//     $idProduto = $_GET["idProduto"] ?? "";
-//     try {
-//       Produto::Remove($pdo, $idProduto);
-//       header("location: produtos.html");
-//     } catch (Exception $e) {
-//       throw new Exception($e->getMessage());
-//     }
-//     break;
-
-//   case "listarProdutos":
-//     try {
-//       $arrayProdutos = Produto::GetFirst30($pdo);
-//       header('Content-Type: application/json; charset=utf-8');
-//       echo json_encode($arrayProdutos);
-//     } catch (Exception $e) {
-//       throw new Exception($e->getMessage());
-//     }
-//     break;
 
   default:
     exit("Ação não disponível");
